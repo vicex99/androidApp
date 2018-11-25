c5c8730e39ebad13998c9f460b532936ddca9390
vicex99 <v.a.lantaron@gmail.com>
2018-11-25 17:48:58 +0100

    login finished

--- app/src/main/java/com/utad/misapuntesclase/activities/LoginActivity.kt
+++ app/src/main/java/com/utad/misapuntesclase/activities/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.utad.misapuntesclase.activities
 
 import android.content.Intent
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -9,21 +10,34 @@ import android.widget.EditText
 import com.utad.misapuntesclase.R
 
 import android.text.TextWatcher
+import android.util.Patterns
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     // conexions with view
+    lateinit var txtEmail: EditText
+    lateinit var txtPsw: EditText
+    lateinit var btnNext: Button
+    val mailPattern = Patterns.EMAIL_ADDRESS
 
     // TODO("set data in sigleton")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        txtPsw = findViewById(R.id.passwordLogin)
+        txtEmail = findViewById(R.id.mailLogin)
+        btnNext = findViewById(R.id.btnLogin)
+
+        btnNext.setTextColor(resources.getColor(R.color.grey))
+        title="U-Tad Blackboard"
+        txtWatch(txtEmail)
+        txtWatch(txtPsw)
     }
 
-    /*
-     *
-     */
     fun onGoNext(view: View){
         Intent(this, RegisterActivity::class.java).apply {
 
@@ -35,18 +49,28 @@ class LoginActivity : AppCompatActivity() {
 
     fun txtWatch(txt:EditText){
         txt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (mailPattern.matcher(txtEmail.text.toString()).matches()) {
+                    btnNext.isEnabled = !txtPsw.text.toString().equals("") && txtPsw.text.length > 6
+                    btnNext.setTextColor(resources.getColor(android.R.color.white))
+                } else {
+                    txtEmail.error = "Is not a valid email"
+                    btnNext.isEnabled = false
+                }
+            }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                if (mailPattern.matcher(txtEmail.text.toString()).matches()) {
+                    btnNext.isEnabled = !txtPsw.text.toString().equals("") && txtPsw.text.length > 6
+                    btnNext.setTextColor(resources.getColor(android.R.color.white))
+                } else {
+                    txtEmail.error = "Is not a valid email"
+                    btnNext.isEnabled = false
+                }
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
         })
     }
 }
